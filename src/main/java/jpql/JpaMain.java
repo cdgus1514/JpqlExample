f0d67062a6aa39e47481f10429ce3403d4fca83f
cdgus1514 <37209022+cdgus1514@users.noreply.github.com>
2021-10-04 19:22:34 +0900

    기본문법 정리
    TypedQuery, Query, 파라미터 바인딩

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,7 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,9 +13,39 @@ public class JpaMain {
         tx.begin();
 
         try{
+            /**
+             * 기본문법
+             */
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            // typeQuery
+//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+//            List<Member> resultList = query.getResultList();
+//
+//            // 결과가 여러개인 경우 (없으면 깡통 반환)
+//            for (Member m : resultList) {
+//                System.out.println("m = " + m);
+//            }
+
+//            TypedQuery<Member> query2 = em.createQuery("select m from Member m", Member.class);
+//            // 결과가 1개인 경우 (없으면 NoResultException, 1개 이상이면 NonUniqueResultException)
+//            Member result = query2.getSingleResult();
+//            System.out.println("result = " + result);
+
+            // Query
+//            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+
+            // 파라미터 바인딩(채널링 형식으로 코드작성)
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+            System.out.println("result = " + result.getUsername());
+            System.out.println("result = " + result.getAge());
 
             tx.commit();
         }catch (Exception e) {
